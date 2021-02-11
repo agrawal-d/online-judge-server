fce09dc4d3c693171ccccb0dfc939bafadce5f1b
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2021-02-11 19:08:52 +0530

    Add CORS support.

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // Enable CORS
-app.use((_req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 app.use(
