fcfbe6a23589aa95b395197c75e86aceb1bc0e17
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2021-02-16 22:58:25 +0530

    Add endpoint to update HE credentials.

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { query } from "express-validator";
+import { body, query } from "express-validator";
 import { UserModel } from "../models";
 import { AuthorizedReq } from "../types";
 const router = express.Router();
@@ -28,5 +28,31 @@ router.get("/me", async function (req: AuthorizedReq, res) {
   const me = await UserModel.findOne({ google_id });
   return res.json(me);
 });
+
+router.post(
+  "/me/update-he",
+  body("he_client_secret").exists(),
+  body("he_client_id").exists(),
+  (req: AuthorizedReq, res) => {
+    const google_id = req.user.google_id;
+    const query = { google_id };
+    UserModel.findOneAndUpdate(
+      query,
+      {
+        $set: {
+          he_client_id: req.body.he_client_id,
+          he_client_secret: req.body.he_client_secret,
+        },
+      },
+      undefined,
+      (err, doc) => {
+        if (err) {
+          return res.json({ errors: [err] });
+        }
+        return res.json(doc);
+      }
+    );
+  }
+);
 
 export default router;
