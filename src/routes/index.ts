4397ed98d843af51000e452af5703e3f0017d21c
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2021-02-23 14:17:52 +0530

    CN test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,16 @@ router.get("/", function (req, res) {
   res.json({ staus: "working" });
 });
 
+router.get("/redirect", function (req, res) {
+  res.redirect("/redirect1");
+});
+
+router.get("/redirect1", function (req, res) {
+  res.redirect("/final");
+});
+
+router.get("/final", function (req, res) {
+  res.end("<h1>OK</h1>");
+});
+
 export default router;
