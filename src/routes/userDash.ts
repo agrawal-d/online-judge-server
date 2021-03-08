a1fd8f40bc08eb84e0aeee84aec619b47bd708f1
Gaurang Gupta <guptagaurang00@gmail.com>
2021-03-09 01:42:54 +0530

    Changed How We query userDash. Working now

--- src/routes/userDash.ts
+++ src/routes/userDash.ts
@@ -1,29 +1,17 @@
 import express from "express";
 import { EligibilityModel } from "../models";
-import { UserModel } from "../models";
 import { AuthorizedReq } from "../types";
 const router = express.Router();
 
 router.get("/", async function (req: AuthorizedReq, res) {
-  const google_id = req.query.google_id as string;
-  const user = await UserModel.findOne({ google_id });
-
-  if (user) {
-    const ret = await EligibilityModel.find({ user_id: google_id });
-    if (ret) {
-      return res.json(ret);
-    }
-    return res.json({
-      error: {
-        message: "Dashboard not found",
-      },
-    });
+  const google_id = req.user.google_id as string;
+  const ret = await EligibilityModel.find({ user_id: google_id });
+  if (ret) {
+    return res.json(ret);
   }
-
   return res.json({
     error: {
-      message: "User not found F",
-      // type: "IDK",
+      message: "Dashboard not found",
     },
   });
 });
