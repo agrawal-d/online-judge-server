9e9eb5c045bbfda0fddc781f84ef7dd7cde247fc
Gaurang Gupta <guptagaurang00@gmail.com>
2021-03-19 16:09:16 +0530

    Fixed TA issue

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -18,32 +18,6 @@ router.post("/make-instructor", body("email").isEmail(), validate, async functio
     {
       $set: {
         is_instructor: true,
-        is_ta: false,
-      },
-    },
-    undefined,
-    (err, doc) => {
-      if (err) {
-        return res.json({ errors: [err] });
-      }
-      return res.json(doc);
-    }
-  );
-});
-
-router.post("/make-ta", body("email").isEmail(), validate, async function (req: AuthorizedReq, res) {
-  const user = await UserModel.findOne({ email: req.body.email });
-  if (!user) {
-    return res.json({
-      errors: [{ message: `User with email ${req.body.email} does not exist` }],
-    });
-  }
-
-  UserModel.findOneAndUpdate(
-    { email: req.body.email },
-    {
-      $set: {
-        is_ta: true,
       },
     },
     undefined,
